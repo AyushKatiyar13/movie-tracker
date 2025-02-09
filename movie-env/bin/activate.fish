d42ab2854c5ebbe27cfd33024d414d27d8d51525
AyushKatiyar13 <ayushkatiyar1301@gmail.com>
2025-02-09 19:55:53 +0000

    serializers

--- movie-env/bin/activate.fish
+++ movie-env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ayush/movie-tracker/movie-env
+set -gx VIRTUAL_ENV /home/nkita/movie-tracker/movie-env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
--- movie_tracker/tracking/views.py
+++ movie_tracker/tracking/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from .serializers import CustomUserSerializer
 
-# Create your views here.
+CustomUser = get_user_model()
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()  
+    serializer_class = CustomUserSerializer 
